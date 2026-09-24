29a85af96bd969e9be6ea0d119908e312b32f3af
marcosinca-istea <marcos.inca@istea.com.ar>
2026-09-24 20:53:32 -0300

    fix desktop

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -9,25 +9,31 @@ export const renderNavbar = () => {
     
     navbarContainer.innerHTML = `
         <nav class="navbar navbar-dark bg-dark py-2">
-            <div class="container-fluid d-flex flex-wrap align-items-center justify-content-between">
-                <!-- Marca / Logo -->
-                <a class="navbar-brand d-flex align-items-center me-0" href="#">
-                    <img src="./src/assets/Logo.png" alt="Scorpion Logo" width="30" height="30" class="d-inline-block align-text-top me-2" onerror="this.style.display='none'">
-                    Scorpion
-                </a>
+            <div class="container-fluid">
+                <div class="row w-100 align-items-center g-2 mx-auto">
+                    <!-- Logo / Marca -->
+                    <div class="col-6 col-lg-3 d-flex align-items-center p-0">
+                        <a class="navbar-brand d-flex align-items-center m-0" href="#">
+                            <img src="./src/assets/Logo.png" alt="Scorpion Logo" width="30" height="30" class="d-inline-block align-text-top me-2" onerror="this.style.display='none'">
+                            Scorpion
+                        </a>
+                    </div>
 
-                <!-- Carrito a la derecha en móviles, último en desktop -->
-                <div class="d-flex align-items-center order-lg-last">
-                    <button class="btn-cart" id="btn-open-cart">
-                        <img src="./src/assets/cart.svg" alt="Carrito" class="me-2">
-                        <span class="cart-counter" id="cart-badge">0</span>                        
-                    </button>
+                    <!-- Buscador: Abajo en celular (col-12), en el medio en PC (col-lg-6) -->
+                    <div class="col-12 col-lg-6 order-3 order-lg-2 px-0 px-lg-2">
+                        <form class="w-100 m-0" id="search-form" aria-label="Buscar productos">
+                            <input class="form-control" type="search" placeholder="Buscar productos..." id="search-input" aria-label="Campo de búsqueda">
+                        </form>
+                    </div>
+
+                    <!-- Carrito: Arriba a la derecha en celular (col-6), a la derecha en PC (col-lg-3) -->
+                    <div class="col-6 col-lg-3 order-2 order-lg-3 d-flex justify-content-end p-0">
+                        <button class="btn-cart" id="btn-open-cart">
+                            <img src="./src/assets/cart.svg" alt="Carrito" class="me-2">
+                            <span class="cart-counter" id="cart-badge">0</span>                        
+                        </button>
+                    </div>
                 </div>
-
-                <!-- Buscador siempre visible (ancho completo en móvil, centrado en desktop) -->
-                <form class="d-flex w-100 w-lg-50 my-2 my-lg-0 order-last order-lg-2 mx-lg-auto" id="search-form" aria-label="Buscar productos">
-                    <input class="form-control" type="search" placeholder="Buscar productos..." id="search-input" aria-label="Campo de búsqueda">
-                </form>
             </div>
         </nav>
 
